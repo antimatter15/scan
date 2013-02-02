d5e6ec04f8935c632ab3ddbfde04dd6d92f419c8
antimatter15 <antimatter15@gmail.com>
2013-02-02 13:02:05 -0500

    more config options, added blank.jpg

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Here be the configs!
 
+arduino = '/dev/ttyACM0'
+scanner_name = "LiDE 110" # mah scanner!
 dimensions = (8.5, 11) # standard letter size paper
 resolution = 300 # 300dpi is good
 margins = 0.5 # half an inch, for placement errors, etc
@@ -21,7 +23,7 @@ print "Initializing SANE"
 
 print sane.init()
 
-scanner = sane.open([id for (id, maker, model, desc) in sane.get_devices() if model == "LiDE 110"][0])
+scanner = sane.open([id for (id, maker, model, desc) in sane.get_devices() if model == scanner_name][0])
 
 print "Acquired Scanner", scanner
 
@@ -36,7 +38,7 @@ print scanner['tl_y'] # zero
 print scanner['br_x'] # nonzero
 print scanner['br_y'] # nonzero
 
-ser = serial.Serial('/dev/ttyACM0', 9600)
+ser = serial.Serial(arduino, 9600)
 while True:
 	print "Waiting for lid close event"
 	line = ser.readline()
@@ -49,21 +51,6 @@ while True:
 
 	print "Acquired Output", im
 
-	# narwhal = numpy.array(im.histogram())
-	# mean = numpy.sum(numpy.dot(range(0, 256), narwhal)) / numpy.sum(narwhal)
-	# mean_square = numpy.sum(numpy.dot(numpy.square(range(0, 256)), narwhal)) / numpy.sum(narwhal)
-	# variance = mean_square - numpy.square(mean)
-	# print mean, mean_square, variance, numpy.sqrt(variance)
-	
-	# normal_hist = [numpy.exp(-numpy.square(x - mean)/(2 * variance)) / numpy.sqrt(2 * numpy.pi * variance) for x in range(0, 256)]
-
-	# normal_hist = numpy.array(blank.histogram())
-
-	# diff = narwhal - normal_hist
-	# print numpy.std(diff), numpy.mean(diff), numpy.sum(numpy.square(diff))
-	# print diff, narwhal, normal_hist
-
-	# find da stdev
 	(width, height) = blurred.size
 	n = int(margins * resolution) # half inch margins
 	diff = ImageChops.difference(blurred, blank).crop((n, n, width - n, height - n))
